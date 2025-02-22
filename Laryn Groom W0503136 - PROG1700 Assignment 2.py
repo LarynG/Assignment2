108c6ca2112ec05ca8ca6e39e5d604805046f60f
LarynG <larynmgroom@gmail.com>
2025-02-22 19:50:04 -0400

    Minor modifications and tweaks to wording. Added 'or' to 'if' statements

--- Laryn Groom W0503136 - PROG1700 Assignment 2.py
+++ Laryn Groom W0503136 - PROG1700 Assignment 2.py
@@ -9,13 +9,15 @@ print("Hinterland Clinic - Patient Servicing Console")
 
 # Hard-Coded Definitions
 programRun = 0 # "True/False" for if code/program is running
+
 doc1 = 50      # Clinician Charges
 doc2 = 90
 doc3 = 120
 doc4 = 160
+
 docPrice = 0
 
-# User-Defined Information
+# User-Defined Information Storage
 patient4Proc = input("Is there a Patient to Process? (Say 'Yes', 'Finish', 'No', or 'Stop') ")
 patientName = " "  # Space for temporary value, "Variable" type
 clinicianReq = " " # Space for temporary value, "Variable" type
@@ -25,15 +27,23 @@ int(programRun) + 1
 while programRun = 1
     if patient4Proc[0].lower() == 'y':
         patientName = str(input("Please enter the patient's name: "))
-        clinicianReq = str(input("How many Clinicians will the patient require? (Number range between 1 and 4) "))
-        if clinicianReq == 1:
-            int(docPrice) + doc1
+        clinicianReq = str(input("How many Clinicians will the patient require? (Range of 1 to 4) "))
+        if clinicianReq == 1 or clinicianReq[0:2].lower() == 'on':
+            docPrice + doc1
+        if clinicianReq == 2 or clinicianReq[0:2].lower() == 'tw':
+            docPrice + doc2
+        if clinicianReq == 3 or clinicianReq[0:2].lower() == 'th':
+            docPrice + doc3
+        if clinicianReq == 4 or clinicianReq[0:2].lower() == 'fo':
+            docPrice + doc4
+        else:
+            print("Error - Please enter a valid number entry for doctors required. (Between 1 and 4)")
 
     if patient4Proc[0].lower() == 'n' or 's' or 'f':
         int(programRun) - 1
         print("Thank you! Shutting down...")
 
-    elif patient4Proc[0].lower():
+    else:
         print("Error - Please enter one of the following answers: 'Yes', 'Finish', 'No', or 'Stop'.")
 
 
