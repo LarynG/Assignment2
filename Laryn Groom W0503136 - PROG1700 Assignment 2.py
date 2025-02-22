00611b8cda7e9e490ae2f1122ae95257ebc9b5d6
LarynG <larynmgroom@gmail.com>
2025-02-22 18:49:05 -0400

    Some Hardcoded definitions created

--- Laryn Groom W0503136 - PROG1700 Assignment 2.py
+++ Laryn Groom W0503136 - PROG1700 Assignment 2.py
@@ -7,9 +7,29 @@
 # Introduction Print Statement
 print("Hinterland Clinic - Patient Servicing Console")
 
+# Hard-Coded Definitions
+doc1 = 50  #Clinician Charges
+doc2 = 90
+doc3 = 120
+doc4 = 160
+
 # User-Defined Information
 patient4Proc = input("Is there a Patient to Process? (Say 'Yes', 'Finish', 'No', or 'Stop')")  # Storage for Patients Waiting
+patientName = " " # Space for temporary value, "Variable" type
+clinicianReq = " " # Space for temporary value, "Variable" type
 
-while patients4Proc[0].lower() == 'y':
+if patient4Proc[0].lower() == 'y':
+    patientName = str(input("Please enter the patient's name: "))
+    clinicianReq = str(input("How many Clinicians will the patient require? (Number range between 1 and 4) "))
 
-while patients4Proc[0].lower() == 'n' or 's' or 'f':
+if patient4Proc[0].lower() == 'n' or 's' or 'f':
+    print("Thank you! Shutting down...")
+
+elif patient4Proc[0].lower():
+    print("Error - Please enter one of the following answers: 'Yes', 'Finish', 'No', or 'Stop'.")
+
+
+# Testing Sector - REMEMBER TO REMOVE BEFORE FINAL COMMIT!!
+print(patient4Proc)
+print(patientName)
+print(clinicianReq)
