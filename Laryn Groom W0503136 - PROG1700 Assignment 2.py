26660d54f94d08908814dbc80e272095b0a99ae2
LarynG <larynmgroom@gmail.com>
2025-02-22 18:11:58 -0400

    Initial Commit

---
+++ Laryn Groom W0503136 - PROG1700 Assignment 2.py
@@ -0,0 +1,15 @@
+# Program Title: Hinterland Clinic Patient Console (PROG1700 Assignment 2)
+#     Objective: "Receive input from user, process multiple ‘strings’ based on their values, perform appropriate calculations on numeric data, and output results."
+#        Author: Laryn Groom (W0503136)
+#          Date: 02-21-2025
+#         Notes: Flowchart should be passed in on Brightspace AND the Git Repository. (Not included yet. Will remove this part of the note when available)
+
+# Introduction Print Statement
+print("Hinterland Clinic - Patient Servicing Console")
+
+# User-Defined Information
+patient4Proc = input("Is there a Patient to Process? (Say 'Yes', 'Finish', 'No', or 'Stop')")  # Storage for Patients Waiting
+
+while patients4Proc[0].lower() == 'y':
+
+while patients4Proc[0].lower() == 'n' or 's' or 'f':
