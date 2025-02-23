100f8dd46c6091a56f95f5edb012bc7c4d2503cd
LarynG <larynmgroom@gmail.com>
2025-02-23 14:21:12 -0400

    Modifications and bug fixes. Expanded on code.

--- Laryn Groom W0503136 - PROG1700 Assignment 2.py
+++ Laryn Groom W0503136 - PROG1700 Assignment 2.py
@@ -8,7 +8,7 @@
 print("Hinterland Clinic - Patient Servicing Console")
 
 # Hard-Coded Definitions
-programRun = 0 # "True/False" for if code/program is running
+programRun = 1 # "True/False" for if code/program is running
 
 doc1 = 50      # Clinician Charges
 doc2 = 90
@@ -19,35 +19,89 @@ docPrice = 0
 
 # User-Defined Information Storage
 patient4Proc = input("Is there a Patient to Process? (Say 'Yes', 'Finish', 'No', or 'Stop') ")
-patientName = " "  # Space for temporary value, "Variable" type
-clinicianReq = " " # Space for temporary value, "Variable" type
+patientName = " "     # Space for temporary value, "Variable" type
+clinicianInput = "0" # Space for temporary value, "Variable" type
+clinicianReq = 0
+healthCard = " "
+
+rxCount = 0
+itemCount = 0
+itemCosts = 0
 
 # User-Interactive
-int(programRun) + 1
-while programRun = 1
-    if patient4Proc[0].lower() == 'y':
-        patientName = str(input("Please enter the patient's name: "))
-        clinicianReq = str(input("How many Clinicians will the patient require? (Range of 1 to 4) "))
-        if clinicianReq == 1 or clinicianReq[0:2].lower() == 'on':
-            docPrice + doc1
-        if clinicianReq == 2 or clinicianReq[0:2].lower() == 'tw':
-            docPrice + doc2
-        if clinicianReq == 3 or clinicianReq[0:2].lower() == 'th':
-            docPrice + doc3
-        if clinicianReq == 4 or clinicianReq[0:2].lower() == 'fo':
-            docPrice + doc4
-        else:
-            print("Error - Please enter a valid number entry for doctors required. (Between 1 and 4)")
+while programRun == 1:
 
-    if patient4Proc[0].lower() == 'n' or 's' or 'f':
-        int(programRun) - 1
-        print("Thank you! Shutting down...")
+    if patient4Proc[0].lower() == 'r':
+        patient4Proc = input("Is there another Patient to Process? (Say 'Yes', 'Finish', 'No', or 'Stop') ") 
+
+    if patient4Proc[0].lower() == 'y':
+        patientName = input("Please enter the patient's name: ")
+        clinicianInput = input("How many Clinicians will the patient require? (Range of 1 to 4) ")
+        patient4Proc = "reask"
+        print(patient4Proc)
+
+        if clinicianInput == '1' or clinicianInput[0:2].lower() == 'on':
+            clinicianReq = 1 # Ensures that "on" is changed to 1 for the "while not" below.
+            docPrice = docPrice + doc1
+            #int(clinicianInput) + 1
+
+        elif clinicianInput == "2" or clinicianInput[0:2].lower() == 'tw':
+            int(clinicianReq) == 2
+            docPrice + doc2
+            #int(clinicianInput) = 1
+
+        elif clinicianInput == "3" or clinicianInput[0:2].lower() == 'th':
+            int(clinicianReq) == 3
+            docPrice + doc3
+            #int(clinicianInput) = 1
+
+        elif clinicianInput == "4" or clinicianInput[0:2].lower() == 'fo':
+            int(clinicianReq) == 4
+            docPrice + doc4
+            #int(clinicianInput) = 1
+
+        while not clinicianReq <= 4 and clinicianReq >= 1:
+            print("Error - Please enter a valid number entry for clinicians required.")
+            clinicianInput = input("How many Clinicians will the patient require? (Range of 1 to 4) ")
 
     else:
         print("Error - Please enter one of the following answers: 'Yes', 'Finish', 'No', or 'Stop'.")
+        clinician4Proc = input("Is there a Patient to Process? ")
+
+    if rxCount = 1
+        itemCosts = 
+
+    if rxCount = 2
+
+
+    if rxCount = 3
+
+
+    if rxCount = 4
+
+
+
+    #if healthCard[0:2].lower() == "ns":
+
+    #if healthCard[0:2].lower() == "nb" or "pe" or "nl":
+
+    #if healthCard[0:2].lower() == "us":
+    
+    #if healthCard[0:2].lower() == "on" or "qc" or "mb":
+
+    #if healthCard[0:2].lower() == "bc" or "ab" or "sk":
+
+    #if healthCard[0:2].lower() == "nt" or "nu" or "yt":
+
+    #elif healthCard[0:2].lower():
+
+if patient4Proc[0].lower() == 'n' or 's' or 'f':
+    print("Thank you! Shutting down...")
+    int(programRun) = 0
 
 
 # Testing Sector - REMEMBER TO REMOVE BEFORE FINAL COMMIT!!
 print(patient4Proc)
 print(patientName)
 print(clinicianReq)
+print(docPrice)
